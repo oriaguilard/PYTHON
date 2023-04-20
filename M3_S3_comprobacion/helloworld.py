7642dcb6ba25471121e723df8e0b79cc26204408
oriaguilar <aguilar.orianna@gmail.com>
2023-04-19 20:35:29 -0400

    primera sesion

---
+++ M3_S3_comprobacion/helloworld.py
@@ -0,0 +1,24 @@
+#comenzando con python
+print("hola")
+print (2)
+print (2+1)
+
+if True:
+    print("probando condicion")
+    print("segunda impresion")
+    if True:    print("indentacion en python")
+
+    
+    
+#separacion de bloque de codigo con 2 lines hacia abajo
+print("separacion")
+
+#tipo de datos
+print(type(5))
+print(type(True)) #booleano
+print(type(False)) #booleano
+print(type(2+1j)) #complex
+
+
+
+
---
+++ M3_S3_comprobacion/modulo.py
@@ -0,0 +1,7 @@
+from math import pi as PI_VALUE #importando libreria matematica/pi_value es un alias
+from math import *
+import math #importando la liberia math
+
+print(math.sqrt(2)) #calcula la raiz
+print(math.exp(2))#calcula el exponente de 2
+print(math.pow(2,3))#calculando la potencia de 2 a 3 
