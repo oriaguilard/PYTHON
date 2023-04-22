9892069bc19b7ac9b9d91037a2bb4b0749fe628a
oriaguilar <aguilar.orianna@gmail.com>
2023-04-21 20:11:11 -0400

    inicializando repositorio

---
+++ DEMO/rebound.py
@@ -0,0 +1,11 @@
+#variables
+a= 20
+b = 30
+#calculado
+c = a*b
+#resultado print("texto {}".format(calculo))
+print(c)
+print("el valor es:", c)
+print(f"el valor es: {c}")
+print("el valor es: {} {} {}".format(c, a*b,a))
+print("el valor es: {} ",c)
---
+++ M3_S3_evaluacion/inicio.py
@@ -0,0 +1,15 @@
+'''
+Requerimos hacer el cálculo de la raíz cuadrada de una variable llamada “y” que tiene valor 81,
+utilizando el módulo de la librería math incorporada en Python. El resultado debemos asignarlo a
+una variable que será impresa en pantalla.
+'''
+
+
+import math
+
+#variables
+y= 81
+#calculado
+raiz = math.sqrt(y) #variable llamada raiz. Acumula el dato
+#resultado print("texto {}".format(calculo))
+print(f"La raiz cuadrada de {y} es : {raiz}")
